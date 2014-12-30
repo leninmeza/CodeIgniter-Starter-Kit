0bbd8379a5d212feb80b433323e90ff4a3572276
Lenin Meza <lenin.meza@adapare.com>
2014-12-30 01:46:20 -0600

    Update config.php
    Set encryption key
    http://jeffreybarke.net/tools/codeigniter-encryption-key-generator/

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'J6262C9elRzbfiJiHAn07ULz2iemyNFe';
 
 /*
 |--------------------------------------------------------------------------
