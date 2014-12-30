b6d4fa17ad93f865c7fa1dce80b36b19b7195b4e
Lenin Meza <lenin.meza@adapare.com>
2014-12-30 01:41:33 -0600

    Update autoload.php
    Add auto libraries, helpers and user config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,8 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
-
+$autoload['libraries'] = array('database' ,'session', 'ion_auth', 'acl', 'form_validation', 'email', 'user_agent', 'smarty');
 
 /*
 | -------------------------------------------------------------------
@@ -64,7 +63,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('user','url','file','form');
 
 
 /*
@@ -80,7 +79,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('user');
 
 
 /*
